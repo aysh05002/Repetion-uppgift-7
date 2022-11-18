bd7d792d6ac8c5f55e7507208c6716edf1156594
aysh05002 <113014238+aysh05002@users.noreply.github.com>
2022-11-18 09:28:41 +0100

    Update Repetion_uppgift_7.py

--- Repetion uppgift 7/Repetion_uppgift_7.py
+++ Repetion uppgift 7/Repetion_uppgift_7.py
@@ -1,5 +1,8 @@
-print("OBS: mata in minst 100, och mata in bara hundratal och tusental")
+print("OBS: mata in minst 100, och mata in bara hundratal och/eller tusental")
 pengar=int(input("Mata in hur manga hundra kronor du vill ha: "))
+
+x=pengar%100
+
 femhundra=pengar%500
 fem=(pengar-femhundra)/500
 
@@ -9,8 +12,9 @@ tva=(femhundra-tvahundra)/200
 etthundra=tvahundra%100
 ett=(tvahundra-etthundra)/100
 
-
-if fem==0 and tva==0:
+if x>0:
+    print("Du har inte matat in hundratal")
+elif fem==0 and tva==0 and ett==1:
     print("Du kommer att fa utt etthundra lapp")
 elif fem==0 and tva>0 and ett==0:
     print("Du kommer att fa utt", tva, "tvahundra lappar")
@@ -24,5 +28,3 @@ elif fem>0 and tva>0 and ett==0:
     print("Du kommer att fa utt", fem, "femhundra lappar och", tva, "tvahundra lappar")
 elif fem>0 and tva>0 and ett==1:
     print("Du kommer fa ut", fem, "femhundra lappar,", tva, "tvahundra lappar och", ett, "hundra lapp")
-else:
-    print("Du har mattat in mindre an hundratal")
